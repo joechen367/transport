b0ac648affdd4d706878442229531e23345616a7
陈文伟 <115938101@qq.com>
2024-03-05 16:46:40 +0800

    version v1.0.4

---
+++ broker/mqtt/options.go
@@ -0,0 +1,112 @@
+package mqtt
+
+import "github.com/joechen367/transport/broker"
+
+///
+/// Option
+///
+
+type cleanSessionKey struct{}
+type authKey struct{}
+type clientIdKey struct{}
+type autoReconnectKey struct{}
+type resumeSubsKey struct{}
+type orderMattersKey struct{}
+type errorLoggerKey struct{}
+type criticalLoggerKey struct{}
+type warnLoggerKey struct{}
+type debugLoggerKey struct{}
+type loggerKey struct{}
+
+type AuthRecord struct {
+	Username string
+	Password string
+}
+
+type LoggerOptions struct {
+	Error    bool
+	Critical bool
+	Warn     bool
+	Debug    bool
+}
+
+// WithCleanSession enable clean session option
+func WithCleanSession(enable bool) broker.Option {
+	return broker.OptionContextWithValue(cleanSessionKey{}, enable)
+}
+
+// WithAuth set username & password options
+func WithAuth(username string, password string) broker.Option {
+	return broker.OptionContextWithValue(authKey{}, &AuthRecord{
+		Username: username,
+		Password: password,
+	})
+}
+
+// WithClientId set client id option
+func WithClientId(clientId string) broker.Option {
+	return broker.OptionContextWithValue(clientIdKey{}, clientId)
+}
+
+// WithAutoReconnect enable aut reconnect option
+func WithAutoReconnect(enable bool) broker.Option {
+	return broker.OptionContextWithValue(autoReconnectKey{}, enable)
+}
+
+// WithResumeSubs .
+func WithResumeSubs(enable bool) broker.Option {
+	return broker.OptionContextWithValue(resumeSubsKey{}, enable)
+}
+
+// WithOrderMatters .
+func WithOrderMatters(enable bool) broker.Option {
+	return broker.OptionContextWithValue(orderMattersKey{}, enable)
+}
+
+func WithErrorLogger() broker.Option {
+	return broker.OptionContextWithValue(errorLoggerKey{}, true)
+}
+
+func WithCriticalLogger() broker.Option {
+	return broker.OptionContextWithValue(criticalLoggerKey{}, true)
+}
+
+func WithWarnLogger() broker.Option {
+	return broker.OptionContextWithValue(warnLoggerKey{}, true)
+}
+
+func WithDebugLogger() broker.Option {
+	return broker.OptionContextWithValue(debugLoggerKey{}, true)
+}
+
+func WithLogger(opt LoggerOptions) broker.Option {
+	return broker.OptionContextWithValue(loggerKey{}, opt)
+}
+
+///
+/// SubscribeOption
+///
+
+type qosSubscribeKey struct{}
+
+// WithSubscribeQos QOS
+func WithSubscribeQos(qos byte) broker.SubscribeOption {
+	return broker.SubscribeContextWithValue(qosSubscribeKey{}, qos)
+}
+
+///
+/// PublishOption
+///
+
+type qosPublishKey struct{}
+type retainedPublishKey struct{}
+
+// WithPublishQos QOS
+func WithPublishQos(qos byte) broker.PublishOption {
+	return broker.PublishContextWithValue(qosPublishKey{}, qos)
+}
+
+// WithPublishRetained retained
+func WithPublishRetained(qos byte) broker.PublishOption {
+	return broker.PublishContextWithValue(retainedPublishKey{}, qos)
+}
---
+++ broker/mqtt/subscriber.go
@@ -0,0 +1,62 @@
+package mqtt
+
+import (
+	"sync"
+
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+	"github.com/joechen367/transport/broker"
+)
+
+type subscriber struct {
+	sync.RWMutex
+
+	options broker.SubscribeOptions
+	m       *mqttBroker
+
+	closed bool
+	topic  string
+	qos    byte
+
+	callback MQTT.MessageHandler
+}
+
+func (s *subscriber) Options() broker.SubscribeOptions {
+	s.RLock()
+	defer s.RUnlock()
+
+	return s.options
+}
+
+func (s *subscriber) Topic() string {
+	s.RLock()
+	defer s.RUnlock()
+
+	return s.topic
+}
+
+func (s *subscriber) Unsubscribe() error {
+	s.Lock()
+	defer s.Unlock()
+
+	var err error
+
+	if s.m != nil && s.m.client != nil {
+		token := s.m.client.Unsubscribe(s.topic)
+		err = token.Error()
+	}
+
+	s.closed = true
+
+	if s.m != nil && s.m.subscribers != nil {
+		_ = s.m.subscribers.Remove(s.topic)
+	}
+
+	return err
+}
+
+func (s *subscriber) IsClosed() bool {
+	s.RLock()
+	defer s.RUnlock()
+
+	return s.closed
+}
